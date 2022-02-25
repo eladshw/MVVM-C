0f062747c4836af7d733b8845f25ecb4ae3c59a9
Elad-GiniApps <elads@gini-apps.com>
2022-02-25 10:41:06 +0200

    Initial Commit

---
+++ Elad'sproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Elad'sproject
+//
+//  Created by elad schwartz on 25/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
